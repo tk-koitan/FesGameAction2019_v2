d0f30124436417c4ee4d69ce8530a046e55dcfeb
小井田大賀 <tk_kakutasu@yahoo.co.jp>
2019-03-15 15:09:55 +0900

    Rigidbody2D修正

--- Assets/Scripts/KoitanLib/RBTest.cs
+++ Assets/Scripts/KoitanLib/RBTest.cs
@@ -13,9 +13,15 @@ public class RBTest : MonoBehaviour
     public bool isGround;
 
     private Vector2 v = Vector2.zero;
+    private Vector2 power = Vector2.zero;
     private Vector2 groundPoint;
     private Vector2 groundNormal;
     public float slopeMaxDeg = 50;
+
+    private bool isContactRight;
+    private bool isContactLeft;
+    private bool isContactUp;
+
 
     [SerializeField] ContactFilter2D filter2d;
     [SerializeField] ContactPoint2D[] contacts;
@@ -32,58 +38,96 @@ public class RBTest : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            v.x += accelVx;
-            v.x = Mathf.Min(v.x, maxVx);
+            power.x += accelVx;
+            power.x = Mathf.Min(power.x, maxVx);
         }
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
-            v.x -= accelVx;
-            v.x = Mathf.Max(v.x, -maxVx);
+            power.x -= accelVx;
+            power.x = Mathf.Max(power.x, -maxVx);
         }
         else
         {
-            v.x *= 0.8f;
+            power.x *= 0.8f;
+        }
+
+        if(isContactRight)
+        {
+            power.x = Mathf.Min(power.x, 0);
+        }
+
+        if(isContactLeft)
+        {
+            power.x = Mathf.Max(power.x, 0);
         }
 
         if (Input.GetKeyDown(KeyCode.Space) && isGround)
         {
-            v.y = maxVy;
+            power.y = maxVy;
             isGround = false;
         }
 
         if (isGround)
         {
-            v.y = 0;
-            v = Vector3.ProjectOnPlane(v, groundNormal);
+            power.y = 0;
+            if(power.magnitude<0.01f)
+            {
+                v = Vector2.zero;
+            }
+            v = Vector3.ProjectOnPlane(power, groundNormal);
             Debug.DrawRay(groundPoint, groundNormal, Color.red);
-            Debug.DrawRay(groundPoint, v, Color.red);
+            //Debug.DrawRay(groundPoint, v, Color.red);
         }
         else
         {
-            v.y += gravity;
+            power.y += gravity;
+            v = power;
         }
 
         //rb.velocity = v;
         rb.MovePosition(transform.position + (Vector3)v);
+        Debug.DrawRay(transform.position, v, Color.white);
+        Debug.Log("速さ:"+v.magnitude);
     }
 
     private void OnCollisionStay2D(Collision2D collision)
     {
-        if (v.y > 0) return;
+        if (v.y > 0 && !isGround) return;
+        isContactRight = false;
+        isContactLeft = false;
+
         groundPoint = collision.contacts[0].point;
         groundNormal = collision.contacts[0].normal;
+        Vector2 center = transform.position;
+        float dir = (v  + center - collision.contacts[0].point).magnitude;
 
         float slopeMinY = Mathf.Sin((90 - slopeMaxDeg) * Mathf.Deg2Rad);
         for (int i = 1; i < collision.contacts.Length; i++)
         {
-            if(collision.contacts[i].point.y < groundPoint.y)
+            float tmpDir = (v + center - collision.contacts[i].point).magnitude;
+            if(tmpDir < dir)
             {
                 groundPoint = collision.contacts[i].point;
                 groundNormal = collision.contacts[i].normal;
             }
+            Debug.DrawRay(collision.contacts[i].point, collision.contacts[i].normal);
         }
 
-        if(groundNormal.y >= slopeMinY)
+        /*
+        RaycastHit2D hit = Physics2D.Raycast(center, Vector2.down * (v.magnitude + box.bounds.size.y));
+        Debug.DrawRay(center, Vector2.down * (v.magnitude + box.bounds.size.y));
+        if (hit)
+        {
+            float tmpDir = (v + center - hit.point).magnitude;
+            if (tmpDir < dir)
+            {
+                groundPoint = hit.point;
+                groundNormal = hit.normal;
+            }
+        }
+        */
+
+        if (groundNormal.y >= slopeMinY)
         {
             isGround = true;
             Debug.DrawRay(groundPoint, groundNormal);
@@ -92,11 +136,21 @@ public class RBTest : MonoBehaviour
         else
         {
             isGround = false;
+            if(groundNormal.x<0)
+            {
+                isContactRight = true;
+            }
+            else if(groundNormal.x>0)
+            {
+                isContactLeft = true;
+            }
+            groundNormal = Vector2.up;
         }
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-        isGround = false;
+        isGround = isContactLeft = isContactRight = false;
+        groundNormal = Vector2.up;
     }
 }
