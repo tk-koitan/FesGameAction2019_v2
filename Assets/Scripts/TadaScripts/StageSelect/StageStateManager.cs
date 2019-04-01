8fb6f0b5a75a455c3f6931b7addd106cfb6fac89
taisei <tatata0389@gmail.com>
2019-04-01 23:11:06 +0900

    ステージセレクトのプログラム面がほぼ完成しました

--- Assets/Scripts/TadaScripts/StageSelect/StageStateManager.cs
+++ Assets/Scripts/TadaScripts/StageSelect/StageStateManager.cs
@@ -9,20 +9,20 @@ using UnityEngine.SceneManagement;
 public class StageStateManager : MonoBehaviour
 {
     //public static StageStateManager instance;
-
-    public static int stageId = 0;
+    public static int nowStageId = 0;
     private StageState nowStageState;
-    public StageState firstStageState;
     [SerializeField]
     private TextMeshProUGUI stageNameText;
     [SerializeField]
     private Image stageImage;
     [SerializeField]
     private Image stageImageFlame;
+    [SerializeField]
+    private GameObject stageObjectList;
 
-    public Vector2 homing = new Vector2(0.2f,0.2f);
+    public Vector2 moveHoming = new Vector2(0.2f,0.2f);
     public float stageAnimSpeed = 1.0f;
-    private float pX, pY;
+    private float playerX, playerY;
     private float targetX, targetY;
 
     // Start is called before the first frame update
@@ -31,11 +31,21 @@ public class StageStateManager : MonoBehaviour
         //if (instance == null) instance = this;
         //else Destroy(gameObject);
 
-        nowStageState = firstStageState;
+        // すべてのstageStateから現在のステージ場所を全探索する テーブル作るより楽で簡潔になった
+        foreach (Transform child in stageObjectList.transform) // 少し重そうなのが欠点
+        {
+            if (nowStageId == child.GetComponent<StageState>().stageId)
+            {
+                nowStageState = child.GetComponent<StageState>();
+                break;
+            }
+        }
+
         ChangeStageInfo();
-        //Debug.Log("Set first Stage Stage");
 
         transform.position = nowStageState.stageTransform.position;
+        // ついでにカメラの位置も変える
+        Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y, -10f);
 
         //DontDestroyOnLoad(this);
     }
@@ -44,27 +54,29 @@ public class StageStateManager : MonoBehaviour
     void Update()
     {
         //Debug.Log(nowStageState.stageName);
-        pX = transform.position.x;
-        pY = transform.position.y;
-        pX = Mathf.Lerp(transform.position.x, targetX, homing.x);
-        pY = Mathf.Lerp(transform.position.y, targetY, homing.y);
+        playerX = transform.position.x;
+        playerY = transform.position.y;
+        playerX = Mathf.Lerp(transform.position.x, targetX, moveHoming.x);
+        playerY = Mathf.Lerp(transform.position.y, targetY, moveHoming.y);
 
-        transform.position = new Vector3(pX, pY, 0f);
+        transform.position = new Vector3(playerX, playerY, 0f);
 
 
-        if (Input.GetKeyDown(KeyCode.Return))
+        if (IsStageSpriteEnabled())
         {
-            if (IsStageSpriteEnabled())
+            if (Input.GetKeyDown(KeyCode.Return))
             {
                 GoNextStage();
                 return;
             }
+            else if (Input.GetKeyDown(KeyCode.Backspace))
+            {
+                BlindStageSprite();
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
             DisplayStageSprite();
-        }
-        else if (Input.GetKeyDown(KeyCode.Backspace))
-        {
-            BlindStageSprite();
-        }
 
         if (IsStageSpriteEnabled()) return;
 
@@ -89,8 +101,11 @@ public class StageStateManager : MonoBehaviour
         ChangeStageInfo();
     }
 
+    // ステージのステート変更に伴う変更
     private void ChangeStageInfo()
     {
+        nowStageId = nowStageState.stageId;
+
         stageImage.sprite = nowStageState.stageSprite;
         stageNameText.text = nowStageState.stageName;
 
@@ -98,6 +113,7 @@ public class StageStateManager : MonoBehaviour
         targetY = nowStageState.stageTransform.position.y;
     }
 
+    // ステージの画像を表示する
     private void DisplayStageSprite()
     {
         stageImageFlame.gameObject.SetActive(true);
@@ -109,17 +125,20 @@ public class StageStateManager : MonoBehaviour
             .SetEase(Ease.OutBack);
     }
 
+    // ステージの画像を消す
     private void BlindStageSprite()
     {
         stageImageFlame.rectTransform.DOKill();
         stageImageFlame.gameObject.SetActive(false);
     }
 
+    // ステージの画像が表示されているか
     private bool IsStageSpriteEnabled()
     {
         return stageImageFlame.gameObject.activeSelf;
     }
 
+    // 目的のステージへ遷移する
     private void GoNextStage()
     {
         SceneManager.LoadScene(nowStageState.stageSceneName);
