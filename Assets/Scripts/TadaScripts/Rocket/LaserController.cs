7e50a1a7832534a09610aa484138dd34b6ff64ee
taisei <tatata0389@gmail.com>
2019-05-26 17:27:25 +0900

    最後のシューティングステージにレーザーを追加

--- Assets/Scripts/TadaScripts/Rocket/LaserController.cs
+++ Assets/Scripts/TadaScripts/Rocket/LaserController.cs
@@ -1,31 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using RocketStage;
 
-public class LaserController : MonoBehaviour
+namespace RocketStage
 {
-    public float defaultScale = 1.0f;
-    public float speed = 1.0f;
-    public float initSpeed = 3.0f;
-    public float dir = 1.0f;
-
-    private float lifeTime = 3.0f;
-
-    // Start is called before the first frame update
-    void Start()
+    public class LaserController : MonoBehaviour
     {
-        speed *= initSpeed;
+        public float defaultScale = 1.0f;
+        public float speed = 1.0f;
+        public float initSpeed = 3.0f;
+        public float dir = 1.0f;
 
-        transform.localScale = new Vector3(defaultScale, defaultScale, 1.0f);
+        private float lifeTime = 3.0f;
 
-        transform.localEulerAngles = new Vector3(0f, 0f, dir);
+        // Start is called before the first frame update
+        protected virtual void Start()
+        {
+            speed *= initSpeed;
 
-        Destroy(gameObject, lifeTime);
-    }
+            transform.localScale = new Vector3(defaultScale, defaultScale, 1.0f);
 
-    // Update is called once per frame
-    void Update()
-    {
-        transform.position += transform.up * speed * Time.deltaTime;
+            transform.localEulerAngles = new Vector3(0f, 0f, dir);
+
+            Destroy(gameObject, lifeTime);
+        }
+
+        // Update is called once per frame
+        protected virtual void Update()
+        {
+            transform.position += transform.up * speed * Time.deltaTime;
+        }
     }
 }
