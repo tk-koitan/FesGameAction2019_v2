f1c4d8302b4a856aad742865493023846b33378b
taisei <tatata0389@gmail.com>
2019-06-07 12:31:31 +0900

    各ステージを大改修

---
+++ Assets/Scripts/TadaScripts/StaffRoll/NextScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StaffRoll;
+
+namespace StaffRoll
+{
+    public class NextScene : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (ActionInput.GetButtonDown(ButtonCode.Jump))
+            {
+                if(!FadeManager.Instance.isFading)
+                    FadeManager.Instance.LoadScene("Title", 2.0f);
+            }
+        }
+    }
+}
